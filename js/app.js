0b5b21cc26c34a5dc43c02c74a4c6a5d65a0dd2a
ASayidov <abdullajonsayidov@gmail.com>
2023-03-05 16:59:23 +0500

    optimzation time

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ function setTodos() {
 function getTime() {
   const now = new Date()
   const date = now.getDate()
-  const month = now.getMonth()
+  const month = now.getMonth() + 1
   const year = now.getFullYear()
   const hours = now.getHours()
   const minut = now.getMinutes()
